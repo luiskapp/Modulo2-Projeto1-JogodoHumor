6ba810bc1ccd9c24ae554beb1d79ce0aaca2a21d
luiskapp <80328112+luiskapp@users.noreply.github.com>
2022-03-14 18:50:47 -0300

    Projeto

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const btn = document.querySelector("#btn");
+const alterar = document.querySelector("#btn");
 
-btn.addEventListener("click", function () { 
-
-});
+alterar.addEventListener("click", function(){ 
+  const image = document.querySelector("#image");
+  const txt = document.querySelector("#txt");
+  if (alterar.value == 'primeiro') {
+    image.src = './Imagens/monica3.gif' 
+    txt.innerText = 'Falante igual a uma Maritaca!  -bláblaáblaááblaáblá-' 
+    alterar.value = 'segundo'
+  } else if (alterar.value == 'segundo') {
+    image.src = './Imagens/monica4.gif'  
+    txt.innerText = 'Rindo das gracinhas do cebolinha.' 
+    alterar.value = 'terceiro' 
+  } else {
+    image.src = './Imagens/monica1.gif'
+    txt.innerText = 'Furiosa porque o cebolinha chamou de dentuça' 
+    alterar.value = 'primeiro' 
+  }
+})
