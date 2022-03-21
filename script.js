6d47946db674f6e8f775e3dbd21035b67bbeac53
luiskapp <80328112+luiskapp@users.noreply.github.com>
2022-03-21 16:43:31 -0300

    Projeto

--- script.js
+++ script.js
@@ -4,27 +4,27 @@ alterar.addEventListener("click", function(){
   const image = document.querySelector("#image");
   const txt = document.querySelector("#txt");
   if (alterar.value == 'primeiro') {
-    image.src = './Imagens/monica3.gif' 
+    image.src = './img/monica3.gif' 
     txt.innerText = 'Falante igual a uma Maritaca!  -bláblaáblaááblaáblá-' 
     alterar.value = 'segundo'
   } else if (alterar.value == 'segundo') {
-    image.src = './Imagens/monica4.gif'  
+    image.src = './img/monica4.gif'  
     txt.innerText = 'Rindo das gracinhas do cebolinha.' 
     alterar.value = 'terceiro' 
   } else if (alterar.value == 'terceiro') {
-    image.src = './Imagens/monica6.gif'  
+    image.src = './img/monica6.gif'  
     txt.innerText = 'Passeando com o monicão.' 
     alterar.value = 'quarto' 
   } else if (alterar.value == 'quarto') {
-    image.src = './Imagens/monica7.gif'  
+    image.src = './img/monica7.gif'  
     txt.innerText = 'Furiosa com cebolinha.' 
     alterar.value = 'quinto' 
   } else if (alterar.value == 'quinto') {
-    image.src = './Imagens/monica8.gif'  
+    image.src = './img/monica8.gif'  
     txt.innerText = 'Feliz da vida!' 
     alterar.value = 'sexto' 
   } else {
-    image.src = './Imagens/monica1.gif'
+    image.src = './img/monica1.gif'
     txt.innerText = 'Nervosa porque o cebolinha chamou de dentuça' 
     alterar.value = 'primeiro' 
   }
